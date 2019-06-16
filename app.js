327853d30f3915eae77e450b7c884de504376d3c
elicarcar <elisabetta.carcar@hotmail.com>
2019-06-16 14:38:42 +0200

    new function

---
+++ app.js
@@ -0,0 +1,45 @@
+//objects
+//item that has properties
+//Key Value
+
+const newUser = {
+    userName = 'Nick', //string
+    age: 29, //number
+    friends: [], //Array
+    hasAcces: false, //boolean
+    pet: [
+        {
+        type: "cat",
+        age: 2,    
+        },
+        {
+        type: "turtle",
+        age: 1001,    
+        }
+    ]
+}
+
+//make an objcet hyfClass19
+// one of bool, string, func, array, number
+
+const hyfClass22 = {
+    studentStatus:'Beginner', //string
+    totalStudents: 16, // number
+    areDeveloppers: false, // bool
+    subjects: [{
+        week1 : 'HTML - CSS',
+    },
+    {
+        week2 : 'CLI',
+    },
+    {
+        week3 : 'Javascript1',
+    }
+    ]// Array of Obj
+
+    oneLastThing: function(){
+        console.log('Hey!');
+    }
+
+};
+
